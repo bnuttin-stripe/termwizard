badab79af6ad9d6ce105abcaf37868cd72bd4c52
bnuttin-stripe <82410533+bnuttin-stripe@users.noreply.github.com>
2024-09-25 14:50:26 -0700

    Incremental updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Papa from 'papaparse';
 import './index.css';
+import Clippy from './clippy.jpg';
 
 export default function App() {
     const [matrix, setMatrix] = useState([]);
@@ -11,7 +12,7 @@ export default function App() {
     const [offlineMode, setOfflineMode] = useState('');
     const [screen, setScreen] = useState('');
     const [connectivity, setConnectivity] = useState('');
-    const [recommendation, setRecommendation] = useState([]);
+    const [recommendation, setRecommendation] = useState({});
 
     const getChoices = (question) => {
         let output = [];
@@ -42,14 +43,13 @@ export default function App() {
     }, []);
 
     const generateRecommendation = () => {
-        setRecommendation([]);
+        setRecommendation({});
         for (let i = 0; i < matrix.length; i++) {
             let row = matrix[i];
             if (row['POS Platform'] == posPlatform && row['Offline Mode'] == offlineMode && row['Screen'] == screen && row['Connectivity'] == connectivity) {
                 setRecommendation(row);
             }
         }
-        //setRecommendation(recommendation);
     }
 
     useEffect(() => {
@@ -60,33 +60,39 @@ export default function App() {
     return (
         <div className="container pt-5">
             <div className="col-6 offset-3">
+                <div className="row mb-4">
+                    <div className="d-flex justify-content-between">
+                        <img src={Clippy} alt="Clippy" className="clippy" />
+                        <div className="banner">It looks like you're trying to integrate Terminal - can I help you?</div>
+                    </div>
+                </div>
                 <div className="form-group row mb-2">
                     <label className="col-sm-6 col-form-label">Is your POS custom or Off the Shelf?</label>
-                    <div class="col-sm-6">
+                    <div className="col-sm-6">
                         <select className="form-control " value={posType} onChange={e => setPOSType(e.target.value)}>
                             <option value="">-- Pick one -- </option>
                             <option value="Custom">Custom</option>
                             <option value="COTS">Commercial product</option>
                         </select>
                     </div>
-
                 </div>
 
+                {posType == 'Custom' && <>
                 <div className="form-group row mb-2">
                     <label className="col-sm-6 col-form-label">What platform is your POS?</label>
-                    <div class="col-sm-6">
-                    <select className="form-control" value={posPlatform} onChange={e => setPosPlatform(e.target.value)}>
-                        <option value="">-- Pick one -- </option>
-                        {getChoices('POS Platform').map((choice, index) => {
-                            return <option key={index} value={choice}>{choice}</option>
-                        })}
-                    </select>
+                    <div className="col-sm-6">
+                        <select className="form-control" value={posPlatform} onChange={e => setPosPlatform(e.target.value)}>
+                            <option value="">-- Pick one -- </option>
+                            {getChoices('POS Platform').map((choice, index) => {
+                                return <option key={index} value={choice}>{choice}</option>
+                            })}
+                        </select>
                     </div>
                 </div>
 
                 <div className="form-group row mb-2">
                     <label className="col-sm-6 col-form-label">Do you need offline mode?</label>
-                    <div class="col-sm-6">
+                    <div className="col-sm-6">
                         <select className="form-control" value={offlineMode} onChange={e => setOfflineMode(e.target.value)}>
                             <option value="">-- Pick one -- </option>
                             {getChoices('Offline Mode').map((choice, index) => {
@@ -98,7 +104,7 @@ export default function App() {
 
                 <div className="form-group row mb-2">
                     <label className="col-sm-6 col-form-label">Do you need a screen?</label>
-                    <div class="col-sm-6">
+                    <div className="col-sm-6">
                         <select className="form-control" value={screen} onChange={e => setScreen(e.target.value)}>
                             <option value="">-- Pick one -- </option>
                             {getChoices('Screen').map((choice, index) => {
@@ -110,7 +116,7 @@ export default function App() {
 
                 <div className="form-group row mb-2">
                     <label className="col-sm-6 col-form-label">How will you connect to the POI?</label>
-                    <div class="col-sm-6">
+                    <div className="col-sm-6">
                         <select className="form-control" value={connectivity} onChange={e => setConnectivity(e.target.value)}>
                             <option value="">-- Pick one -- </option>
                             {getChoices('Connectivity').map((choice, index) => {
@@ -120,23 +126,44 @@ export default function App() {
                     </div>
                 </div>
 
-                <div className="row recommendation">
+                {Object.keys(recommendation).length > 0 && <div className="row alert alert-primary mt-4"  style={{margin: 0}}>
                     {recommendation["Reader - mPOS"] == "x" && <div>Reader - mPOS</div>}
                     {recommendation["Reader - sPOS"] == "x" && <div>Reader - sPOS</div>}
                     {recommendation["Reader - Tap-to-pay"] == "x" && <div>Reader - Tap-to-pay</div>}
                     {recommendation["Reader - AoD"] == "x" && <div>Reader - AoD</div>}
-                    {recommendation["Integration - SDI"] == "x" && <div>Integration - SDI</div>}
-                    {recommendation["Integration - JavaScript"] == "x" && <div>Integration - JavaScript</div>}
-                    {recommendation["Integration - iOS"] == "x" && <div>Integration - iOS</div>}
-                    {recommendation["Integration - Android"] == "x" && <div>Integration - Android</div>}
-                    {recommendation["Integration - .NET"] == "beta" && <div>Integration - .NET (beta)</div>}
-                    {recommendation["Integration - Java"] == "beta" && <div>Integration - Java (beta)</div>}
+                    {recommendation["Integration - Server Driven"] == "x" && <div>Integration - SDI</div>}
+                    {recommendation["Integration - JavaScript SDK"] == "x" && <div>Integration - JavaScript</div>}
+                    {recommendation["Integration - iOS SDK"] == "x" && <div>Integration - iOS</div>}
+                    {recommendation["Integration - Android SDK"] == "x" && <div>Integration - Android</div>}
+                    {recommendation["Integration - .NET SDK"] == "beta" && <div>Integration - .NET (beta)</div>}
+                    {recommendation["Integration - Java SDK"] == "beta" && <div>Integration - Java (beta)</div>}
                     {recommendation["Connectivity - Ethernet"] == "x" && <div>Connectivity - Ethernet</div>}
                     {recommendation["Connectivity - USB"] == "x" && <div>Connectivity - USB</div>}
                     {recommendation["Connectivity - WiFi"] == "x" && <div>Connectivity - WiFi</div>}
                     {recommendation["Connectivity - Cellular"] == "x" && <div>Connectivity - Cellular</div>}
                     {recommendation["Connectivity - Bluetooth"] == "x" && <div>Connectivity - Bluetooth</div>}
-                </div>
+                </div>}
+                </>}
+                
+
+
+
+
+
+
+
+
+                {posType == 'COTS' && 
+                <div className="row alert alert-primary mt-4" style={{margin: 0}}>
+                    We have a number of pre-built connectors and partners that can help with your integration. Please contact Sales for more information.
+                </div>}
+
+                {recommendation["Warning"] &&  
+                <div className="row mt-4 alert alert-warning" style={{margin: 0}}>
+                    {recommendation["Warning"] == "Silly" && <div>This is a bit of an unusual set up, but go for it!</div>}
+                    {recommendation["Warning"] == "Offline Issue" && <div>If your POS is web based, offline mode is not be applicable</div>}
+                    {recommendation["Warning"] == "Internal Only" && <div>This setup </div>}
+                </div>}
             </div>
         </div>
     )
